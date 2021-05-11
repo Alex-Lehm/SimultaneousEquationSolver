7ba5f74c05294572bef128b86ad271b9758e9f9f
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-11 13:03:01 +0100

    Create skeleton of main class

---
+++ main.py
@@ -0,0 +1,13 @@
+class SimultaneousEquationSolver:
+
+    def __init__(self):
+        pass
+
+    def input_values(self):
+        pass
+
+    def amend_values(self):
+        pass
+
+    def calculate(self):
+        pass

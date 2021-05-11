d54781c2ca96eaecd0da93325839383ea65bf54d
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-11 13:20:53 +0100

    Implement __init__() and input_values() to set up variables and take input from the user, displaying equation format for clear inputs

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 class SimultaneousEquationSolver:
 
     def __init__(self):
-        pass
+        self.x, self.y = None, None
+        self.eqn1 = [0, 0, 0]
+        self.eqn2 = [0, 0, 0]
+        self.result = [0, 0, 0]
 
     def input_values(self):
-        pass
+        print("""Please enter values in the following format:
+                          ax + by = c1
+                          dx + ey = c2""")
+
+        self.eqn1[0] = int(input("Enter value a: "))
+        self.eqn1[1] = int(input("Enter value b: "))
+        self.eqn1[2] = int(input("Enter value c1: "))
+        self.eqn2[0] = int(input("Enter value d: "))
+        self.eqn2[1] = int(input("Enter value e: "))
+        self.eqn2[2] = int(input("Enter value c2: "))
 
     def amend_values(self):
         pass

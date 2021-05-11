756b1c60fd8f2a43bedbbad10e3a2c7aab46f28e
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-11 13:06:11 +0100

    Create formulae.py with highest_common_factor() implemented

---
+++ formulae.py
@@ -0,0 +1,18 @@
+def highest_common_factor(a, b):
+    """Method to calculate the highest common factor of two values, using the modulus form of Euclid's
+        algorithm:
+        1. Compare the values. Take the modulus of the two: highest % lowest.
+        2. Check if either value is 0.
+        3. If not, repeat 1.
+        4. If yes, return the non-zero value."""
+
+    if a == b:
+        return a
+
+    while a > 0 and b > 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+
+    return a if b == 0 else b

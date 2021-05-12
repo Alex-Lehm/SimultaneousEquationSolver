8ca4c9bbd1370c8625a03ddd92c614b788f2bf3f
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-12 15:22:55 +0100

    Create docstrings for class and methods of SimEqnSolver.py

--- SimEqnSolver.py
+++ SimEqnSolver.py
@@ -5,14 +5,27 @@ from formulae import lowest_common_multiple
 
 
 class SimultaneousEquationSolver:
+    """Class to represent a dedicated object to solving (by elimination) linear, 2D simultaneous equations,
+    for example:
+
+    3x + 2y = 4
+
+    2x + 3y = 6"""
 
     def __init__(self):
+        """Constructor method, creates the following instance variables:
+
+        x, y -- the calculated values of the unknowns
+        eqn1, eqn2 -- lists holding the known values for each equation in the form [0]x + [1]y = [2]
+        result -- the result of elimination between eqn1 and eqn2"""
         self.x, self.y = None, None
         self.eqn1 = [0, 0, 0]
         self.eqn2 = [0, 0, 0]
         self.result = [0, 0, 0]
 
     def input_values(self):
+        """Method to take user input for the known values"""
+
         print("""Please enter values in the following format:
                           ax + by = c1
                           dx + ey = c2""")
@@ -25,6 +38,9 @@ class SimultaneousEquationSolver:
         self.eqn2[2] = int(input("Enter value c2: "))
 
     def amend_values(self):
+        """Method to prepare equations for the elimination step, and carry out elimination.
+        Calculates the necessary multiplier for each equation."""
+
         # calculate what to multiply the equations by for elimination
         lcm = lowest_common_multiple(self.eqn1[0], self.eqn2[0])
         multiplier1 = lcm / self.eqn1[0]
@@ -48,6 +64,11 @@ class SimultaneousEquationSolver:
                 self.result[i] = self.eqn1[i] + self.eqn2[i]
 
     def calculate_unknowns(self):
+        """Method to calculate the unknown values using the result of elimination between the two
+        equations.
+
+        Throws a ZeroDivisionError if there are no or infinite solutions to the equations."""
+
         self.amend_values()
 
         # if there are no/infinite solutions, there will be a division by zero.

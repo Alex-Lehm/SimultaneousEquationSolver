c90b1cfdbe809805fa1987c47a048b9281560587
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-12 16:21:06 +0100

    Restructure run.py to use demo() function and handle user input

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
 from SimEqnSolver import SimultaneousEquationSolver
 
-if __name__ == "__main__":
+
+def demo():
     solver = SimultaneousEquationSolver()
-    solver.input_values()
+
+    print("""Please enter values in the following format:
+                                  ax + by = c1
+                                  dx + ey = c2""")
+
+    eqn1, eqn2 = [0, 0, 0], [0, 0, 0]
+
+    eqn1[0] = int(input("Enter value a: "))
+    eqn1[1] = int(input("Enter value b: "))
+    eqn1[2] = int(input("Enter value c1: "))
+    eqn2[0] = int(input("Enter value d: "))
+    eqn2[1] = int(input("Enter value e: "))
+    eqn2[2] = int(input("Enter value c2: "))
+
+    solver.input_equations(eqn1, eqn2)
     solver.calculate_unknowns()
 
     print("""x = {}
-y = {}""".format(solver.x, solver.y))
+    y = {}""".format(solver.x, solver.y))
+
+
+if __name__ == "__main__":
+    demo()

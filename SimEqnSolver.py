1e5d6dfe9e5129997d727afd82c4f274a69e37fd
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-05-12 16:20:41 +0100

    Remove user input from input_values(), rename input_values() to input_equations()

--- SimEqnSolver.py
+++ SimEqnSolver.py
@@ -23,19 +23,11 @@ class SimultaneousEquationSolver:
         self.eqn2 = [0, 0, 0]
         self.result = [0, 0, 0]
 
-    def input_values(self):
+    def input_equations(self, eqn1, eqn2):
         """Method to take user input for the known values"""
 
-        print("""Please enter values in the following format:
-                          ax + by = c1
-                          dx + ey = c2""")
-
-        self.eqn1[0] = int(input("Enter value a: "))
-        self.eqn1[1] = int(input("Enter value b: "))
-        self.eqn1[2] = int(input("Enter value c1: "))
-        self.eqn2[0] = int(input("Enter value d: "))
-        self.eqn2[1] = int(input("Enter value e: "))
-        self.eqn2[2] = int(input("Enter value c2: "))
+        self.eqn1 = eqn1
+        self.eqn2 = eqn2
 
     def amend_values(self):
         """Method to prepare equations for the elimination step, and carry out elimination.
